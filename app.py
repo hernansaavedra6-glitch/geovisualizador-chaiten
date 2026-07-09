9382c14c2f2acfeb88d78e1c0639f467fdeb1f52
hernansaavedra6-glitch <hernan.saavedra6@gmail.com>
2026-07-08 21:58:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import branca.colormap as bcm
 
 st.set_page_config(page_title="GeoVisualizador Chaitén", layout="wide", page_icon="🌋")
 
-DATA_DIR = Path(__file__).parent / "Datos"
+DATA_DIR = Path(__file__).parent / "data"
 
 # ----------------------------------------------------------------------
 # CARGA DE DATOS SEGUROS
